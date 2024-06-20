00da1eb2a74f06cac63a823f9758b6d71f6c97a9
Sohybe <sohaibibrahim14@gmail.com>
2024-06-20 11:35:13 +0300

    First commit for backup. Nearly finished homepage.

---
+++ app/create/page.tsx
@@ -0,0 +1,10 @@
+import NavHead from "../components/header";
+
+export default function Create(){
+
+    return(
+    <main className="mx-5 h-[100vh]">
+      <NavHead />
+    </main>
+    )
+}
---
+++ app/post/page.tsx
@@ -0,0 +1,10 @@
+import NavHead from "../components/header";
+
+export default function Post() {
+
+    return(
+    <main className="mx-5 h-[100vh]">
+      <NavHead />
+    </main>
+    )
+}
