387f5606373d34de6997ad03663c348aa67c76cb
Sohybe <sohaibibrahim14@gmail.com>
2024-06-22 00:12:08 +0300

    lil change in styles of blogposts

--- app/blogpost/[slug]/page.tsx
+++ app/blogpost/[slug]/page.tsx
@@ -42,13 +42,13 @@ export default function BlogPost() {
   }
 
   return (
-    <main className="min-h-screen flex flex-col mx-5 font-oswald bg-black">
+    <main className="min-h-screen flex flex-col mx-5 font-oswald bg-black relative top-[10vh]">
       <NavHead />
       <div className="flex-grow flex items-center justify-center flex-col py-8">
-        <h1  id="post-item"  className="text-white text-3xl capitalize hover:text-black post-title cursor-default">{post.title}</h1>
-        <p id="post-item"  className="text-white font-bold">by {post.userId}</p>
+        <h1  id="post-item"  className="text-white text-6xl capitalize hover:text-black post-title cursor-default">{post.title}</h1>
+        <p id="post-item"  className="text-white font-bold text-3xl mt-3">by {post.userId}</p>
         <hr id="post-item"  className="my-4" />
-        <div  id="post-item" className="text-white">{post.body}</div>
+        <div  id="post-item" className="text-white text-2xl">{post.body}</div>
       </div>
       <Footer />
     </main>
